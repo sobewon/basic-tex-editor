7eb6f294f62d8f4ee30273c937660bc9bd529a6b
John Magnuson <sobewon@gmail.com>
2023-05-19 04:06:05 -0400

    src-sw.js and webpack config files completed

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,4 +27,16 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // name of the cache storage (in this case i've  named it 'asset-cache')
+    cacheName: 'asset-cache',
+    plugins: [
+      //plugin will cache responses with headers to a max-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
